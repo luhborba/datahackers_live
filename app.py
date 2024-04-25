6a980f2ad662f9a05841e2c67525cd2065a8ebf2
LucianoBorba <luhborbafilho@gmail.com>
2024-04-25 16:22:00 -0300

    refactor: reformatando conforme a biblioteca black

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+
 @st.cache_data
 def carregar_dados():
-    return pd.read_csv('data/vendas.csv')
+    return pd.read_csv("data/vendas.csv")
+
 
 def main():
     st.set_page_config(
@@ -14,29 +16,91 @@ def main():
         page_icon="🛒",
         layout="wide",
     )
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.set_option("deprecation.showPyplotGlobalUse", False)
     st.title("Sistema de Vendas - DH")
-    data_atual = datetime.now().strftime('%d/%m/%Y')
+    data_atual = datetime.now().strftime("%d/%m/%Y")
 
     with st.expander("🛒 Registrar Venda:"):
         with st.form("Registro de Venda"):
-            produto = st.selectbox('Selecione o Produto', ['TV', 'Notebook', 'Celular', 'Tablet', 'PC', 'Geladeira', 'Fogão', 'Microondas'], index=None, placeholder='Escolha o Produto')
-            categoria = st.selectbox('Selecione a Categoria', ['Eletrodomesticos', 'Informática'], index=None, placeholder='Escolha a Categoria')
-            preco = st.number_input('Preço R$', value=0.0)
-            quantidade = st.number_input('Quantidade', value=0)
-            forma_pagamento = st.selectbox('Forma de Pagamento', ['Dinheiro', 'Cartão de Crédito', 'Cartão de Debito', 'Boleto','Pix'], index=None, placeholder='Escolha a Forma de Pagamento')
+            produto = st.selectbox(
+                "Selecione o Produto",
+                [
+                    "TV",
+                    "Notebook",
+                    "Celular",
+                    "Tablet",
+                    "PC",
+                    "Geladeira",
+                    "Fogão",
+                    "Microondas",
+                ],
+                index=None,
+                placeholder="Escolha o Produto",
+            )
+            categoria = st.selectbox(
+                "Selecione a Categoria",
+                ["Eletrodomesticos", "Informática"],
+                index=None,
+                placeholder="Escolha a Categoria",
+            )
+            preco = st.number_input("Preço R$", value=0.0)
+            quantidade = st.number_input("Quantidade", value=0)
+            forma_pagamento = st.selectbox(
+                "Forma de Pagamento",
+                ["Dinheiro", "Cartão de Crédito", "Cartão de Debito", "Boleto", "Pix"],
+                index=None,
+                placeholder="Escolha a Forma de Pagamento",
+            )
             salvar = st.form_submit_button("Registrar Venda")
 
             if salvar:
                 try:
                     df = carregar_dados()
-                    novo_registro = pd.DataFrame([[produto, categoria, preco, quantidade, forma_pagamento, data_atual]], columns=['Produtos', 'Categorias', 'Preço', 'Quantidade', 'Forma de Pagamento', 'DataVenda'])
+                    novo_registro = pd.DataFrame(
+                        [
+                            [
+                                produto,
+                                categoria,
+                                preco,
+                                quantidade,
+                                forma_pagamento,
+                                data_atual,
+                            ]
+                        ],
+                        columns=[
+                            "Produtos",
+                            "Categorias",
+                            "Preço",
+                            "Quantidade",
+                            "Forma de Pagamento",
+                            "DataVenda",
+                        ],
+                    )
                     df_final = pd.concat([df, novo_registro])
-                    df_final.to_csv('data/vendas.csv', index=False)
+                    df_final.to_csv("data/vendas.csv", index=False)
                 except:
-                    df = pd.DataFrame([[produto, categoria, preco, quantidade, forma_pagamento, data_atual]], columns=['Produtos', 'Categorias', 'Preço', 'Quantidade', 'Forma de Pagamento', 'DataVenda'])
-                    df.to_csv('data/vendas.csv', index=False)
-                
+                    df = pd.DataFrame(
+                        [
+                            [
+                                produto,
+                                categoria,
+                                preco,
+                                quantidade,
+                                forma_pagamento,
+                                data_atual,
+                            ]
+                        ],
+                        columns=[
+                            "Produtos",
+                            "Categorias",
+                            "Preço",
+                            "Quantidade",
+                            "Forma de Pagamento",
+                            "DataVenda",
+                        ],
+                    )
+                    df.to_csv("data/vendas.csv", index=False)
+
                 st.success("Venda Registrada com Sucesso!")
 
     with st.expander("Listar Vendas:"):
@@ -44,42 +108,41 @@ def main():
             df = carregar_dados()
             data_filtro = st.date_input("Filtro Data da Venda", value=None)
             if data_filtro:
-                data_filtro = data_filtro.strftime('%d/%m/%Y')
-                df = df[df['DataVenda'] == data_filtro]
+                data_filtro = data_filtro.strftime("%d/%m/%Y")
+                df = df[df["DataVenda"] == data_filtro]
 
             st.dataframe(df)
         except:
             st.warning("Nenhuma Venda Registrada!")
-  
-            
+
     with st.expander("Dashboard Vendas"):
         try:
             df = carregar_dados()
 
             # Gráfico: Distribuição de vendas por categoria
-            vendas_por_categoria = df['Categorias'].value_counts()
+            vendas_por_categoria = df["Categorias"].value_counts()
             plt.figure(figsize=(8, 5))
             plt.bar(vendas_por_categoria.index, vendas_por_categoria.values)
-            plt.title('Distribuição de Vendas por Categoria')
-            plt.xlabel('Categoria')
-            plt.ylabel('Total de Vendas')
+            plt.title("Distribuição de Vendas por Categoria")
+            plt.xlabel("Categoria")
+            plt.ylabel("Total de Vendas")
             plt.xticks(rotation=45)
             st.pyplot()
             st.divider()
 
             # Gráfico: Vendas por produtos
-            vendas_por_produto = df['Produtos'].value_counts()
+            vendas_por_produto = df["Produtos"].value_counts()
             plt.figure(figsize=(8, 5))
             plt.bar(vendas_por_produto.index, vendas_por_produto.values)
-            plt.title('Vendas por Produto')
-            plt.xlabel('Produto')
-            plt.ylabel('Total de Vendas')
+            plt.title("Vendas por Produto")
+            plt.xlabel("Produto")
+            plt.ylabel("Total de Vendas")
             plt.xticks(rotation=45)
             st.pyplot()
             st.divider()
 
             # Gráfico: Total de vendas por dia (usando plotly)
-            vendas_por_dia = df['DataVenda'].value_counts().sort_index()
+            vendas_por_dia = df["DataVenda"].value_counts().sort_index()
             fig = px.line(x=vendas_por_dia.index, y=vendas_por_dia.values, markers=True)
             fig.update_layout(
                 title="Total de Vendas por Dia",
@@ -88,11 +151,12 @@ def main():
                 xaxis=dict(tickangle=-45),
                 width=800,
                 height=500,
-                xaxis_rangeslider_visible=True
+                xaxis_rangeslider_visible=True,
             )
             st.plotly_chart(fig, use_container_width=True)
         except:
             st.warning("Nenhuma Venda Registrada!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- gerador.py
+++ gerador.py
@@ -3,32 +3,72 @@ import random
 from datetime import datetime, timedelta
 
 # Definindo as opções para os selectboxes
-produtos = ['TV', 'Notebook', 'Celular', 'Tablet', 'PC', 'Geladeira', 'Fogão', 'Microondas']
-categorias = ['Eletrodomesticos', 'Informática']
-formas_pagamento = ['Dinheiro', 'Cartão de Crédito', 'Cartão de Debito', 'Boleto','Pix']
+produtos = [
+    "TV",
+    "Notebook",
+    "Celular",
+    "Tablet",
+    "PC",
+    "Geladeira",
+    "Fogão",
+    "Microondas",
+]
+categorias = ["Eletrodomesticos", "Informática"]
+formas_pagamento = [
+    "Dinheiro",
+    "Cartão de Crédito",
+    "Cartão de Debito",
+    "Boleto",
+    "Pix",
+]
 
 # Lista para armazenar os registros
 registros = []
 
 
-data_inicial = datetime.strptime('2024-03-01', '%Y-%m-%d')
-data_final = datetime.strptime('2024-04-25', '%Y-%m-%d')
+data_inicial = datetime.strptime("2024-03-01", "%Y-%m-%d")
+data_final = datetime.strptime("2024-04-25", "%Y-%m-%d")
 delta = data_final - data_inicial
 
 for i in range(delta.days + 1):
     data_atual = data_inicial + timedelta(days=i)
     num_vendas = random.randint(1, 10)  # número aleatório de vendas por dia
     for _ in range(num_vendas):
-        produto = random.choice(['TV', 'Notebook', 'Celular', 'Tablet', 'PC', 'Geladeira', 'Fogão', 'Microondas'])
-        categoria = random.choice(['Eletrodomesticos', 'Informática'])
+        produto = random.choice(
+            [
+                "TV",
+                "Notebook",
+                "Celular",
+                "Tablet",
+                "PC",
+                "Geladeira",
+                "Fogão",
+                "Microondas",
+            ]
+        )
+        categoria = random.choice(["Eletrodomesticos", "Informática"])
         preco = round(random.uniform(50, 2000), 2)
         quantidade = random.randint(1, 10)
-        forma_pagamento = random.choice(['Dinheiro', 'Cartão de Crédito', 'Cartão de Debito', 'Boleto','Pix'])
-        data_venda = data_atual.strftime('%d/%m/%Y')  # Formato DD/MM/YYYY
-        registros.append([produto, categoria, preco, quantidade, forma_pagamento, data_venda])
+        forma_pagamento = random.choice(
+            ["Dinheiro", "Cartão de Crédito", "Cartão de Debito", "Boleto", "Pix"]
+        )
+        data_venda = data_atual.strftime("%d/%m/%Y")  # Formato DD/MM/YYYY
+        registros.append(
+            [produto, categoria, preco, quantidade, forma_pagamento, data_venda]
+        )
 
 # Criando o DataFrame
-df = pd.DataFrame(registros, columns=['Produtos', 'Categorias', 'Preço', 'Quantidade', 'Forma de Pagamento', 'DataVenda'])
+df = pd.DataFrame(
+    registros,
+    columns=[
+        "Produtos",
+        "Categorias",
+        "Preço",
+        "Quantidade",
+        "Forma de Pagamento",
+        "DataVenda",
+    ],
+)
 
 # Salvando o DataFrame em um arquivo CSV
-df.to_csv('data/vendas.csv', index=False)
+df.to_csv("data/vendas.csv", index=False)
